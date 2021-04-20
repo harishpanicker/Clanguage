c8aeda4e6e481a80685b728843b01cde2aefbbb3
harishpanicker <harishpanicker007@gmail.com>
2021-04-20 18:24:03 +0530

    Added recursion program

---
+++ print_natural_number.c
@@ -0,0 +1,23 @@
+/*WAP to print all natural number between 1 to n*/
+#include<stdio.h>  
+  
+void display(int);  
+  
+int main()  
+{
+	int limit;
+	printf("Enter the number of terms to be printed\n");  
+	scanf("%d", &limit);  	      
+	printf("\nNatural Numbers from 1 To %d are:", limit);  
+	display(limit);
+	return 0;  
+}  
+  
+void display(int num)  
+{  
+	if(num)  
+		display(num-1);  
+	else  
+		return;
+	printf("\n%d\n", num);  
+}
---
+++ reverse_number.c
@@ -0,0 +1,26 @@
+/*WAP to reverse a number using recursion*/
+#include<stdio.h>
+int reverse_function(int num);
+int main(){
+   int num,reverse_number;
+
+   //User would input the number
+   printf("\nEnter any number:");
+   scanf("%d",&num);
+
+   //Calling user defined function to perform reverse
+   reverse_number=reverse_function(num);
+   printf("\nAfter reverse the no is :%d",reverse_number);
+   return 0;
+}
+int sum=0,rem;
+int reverse_function(int num){
+   if(num){
+      rem=num%10;
+      sum=sum*10+rem;
+      reverse_function(num/10);
+   }
+   else
+      return sum;
+   return sum;
+}
